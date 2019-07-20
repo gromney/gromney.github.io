a5badceae8251c4aa0361dfaa77fec102730d7d1
Geronimo Romney <g.romney@gmail.com>
2019-07-19 20:46:42 -0400

    menu hover and others changes

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,25 +3,69 @@ import React, { Component } from 'react';
 import './Main.css';
 
 
-const about = (<section id="about"></section>)
-const work = (<section id="work"></section>)
+const about = (
+    <section id="about">
+        <div className="section-header">
+            <h1>about</h1>
+        </div>
+        <div className="section-content">
+            <p>about paragrph</p>
+        </div>
+
+    </section>
+);
+
+const work = (
+    <section id="work">
+        <div className="section-header">
+            <h1>work</h1>
+        </div>
+        <div className="section-content">
+            <p>work paragrph</p>
+        </div>
+    </section>
+);
+
+const skills = (
+    <section id="skills">
+        <div className="section-header">
+            <h1>skills</h1>
+        </div>
+        <div className="section-content">
+            <p>skills paragrph</p>
+        </div>
+    </section>
+);
+
+const hobbies = (
+    <section id="hobbies">
+        <div className="section-header">
+            <h1>hobbies</h1>
+        </div>
+        <div className="section-content">
+            <p>hobbies paragrph</p>
+        </div>
+    </section>
+)
 
 class Main extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        
+
     }
-    
-    render(){
-        return(
+
+    render() {
+        return (
             <div className="main">
                 {about}
                 {work}
+                {skills}
+                {hobbies}
             </div>
         )
     }
-        
-    
+
+
 }
 
 export default Main
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -8,36 +8,42 @@ class Menu extends Component {
         this.state = {
             open: false,
             menuText: 'MENU',
-            visibility: props.visibility
+            isHovering: false
         }
-        this.state.visibility = props.visibility
 
-        // this.showMenu = this.showMenu.bind(this);
-        // this.hideMenu = this.hideMenu.bind(this);
         this.toggleMenu = this.toggleMenu.bind(this);
+        this._onMouseEnter = this._onMouseEnter.bind(this);
+        this._onMouseLeave = this._onMouseLeave.bind(this);
+    }
+
+    _onMouseEnter() {
+        this.setState({isHovering:true});
+
+    }
+    _onMouseLeave(){
+        this.setState({isHovering:false});
     }
 
     toggleMenu() {
         this.setState({ open: !this.state.open });
-        // console.log(this.state.open);
     }
 
-    // showMenu() {
-    //     this.setState({ overlayClass: 'open', menuText: 'CLOSE' });
-    // }
-
-    // hideMenu() {
-    //     this.setState({ overlayClass: 'close', menuText: 'MENU' });
-
-    // }
     render() {
         const visible = this.state.open && 'open';
+        const framedStyle = this.state.isHovering ? 'framed' :'un-framed';
+        console.log(framedStyle);
         return (
-            <div>
+            <div className={`${framedStyle}`}>
 
-                <button 
+                <button
                     className="center"
-                    onClick={this.toggleMenu}>{this.state.menuText}</button>
+                    onClick={this.toggleMenu}
+                    onMouseEnter={this._onMouseEnter}
+                    onMouseLeave={this._onMouseLeave}
+                >
+                    {this.state.menuText} {this.state.isHovering}
+                </button>
+
 
                 <div className={`overlay ${visible}`}>
                     <nav>
@@ -45,12 +51,9 @@ class Menu extends Component {
                             <li><a href="#about" onClick={this.toggleMenu}>ABOUT</a></li>
                             <li><a href="#work" onClick={this.toggleMenu}>WORK</a></li>
                             <li><a href="#skills" onClick={this.toggleMenu}>SKILLS</a></li>
-                            <li><a href="#hobbies"onClick={this.toggleMenu} >HOBBIES</a></li>
+                            <li><a href="#hobbies" onClick={this.toggleMenu} >HOBBIES</a></li>
                         </ul>
                     </nav>
-                </div>
-                <div className="framed" >
-
                 </div>
             </div>
 
